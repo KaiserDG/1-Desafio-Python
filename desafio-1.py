d46fad9477f2d3ab521c4bc64feddac565064867
Gustavo Doré Gonçalves <162833798+KaiserDG@users.noreply.github.com>
2024-03-17 14:49:25 -0300

    Add files via upload

---
+++ desafio-1.py
@@ -0,0 +1,16 @@
+#1 Desafio
+
+number = int(input("Digite o número\n"))
+
+print(f"O antecessor do número {number} é {number -1} e seu sucessor é {number +1}")
+
+#2 Desafio
+
+note1 = float(input("Digite a primeira nota\n"))
+note2 = float(input("Digite a segunda nota\n"))
+note3 = float(input("Digite a terceira nota\n"))
+note4 = float(input("digite a quarta nota\n"))
+
+avarage = (note1 + note2 + note3 + note4)/4
+
+print(f"A média entre as notas é: {avarage}")
